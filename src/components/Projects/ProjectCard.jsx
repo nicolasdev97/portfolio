3176c32ff430a239175b86ba615becda969baa08
nicolasdev97 <nicolas.po.im@gmail.com>
2025-01-16 12:33:23 -0500

    Se agrega la traduccion a la seccion de projects.

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 import styles from './ProjectCard.module.css';
 import {getImageUrl} from '../../utils.js';
 
-export const ProjectCard = ({project : {title, imageSrc, description, skills, demo, source}}) => {
+export const ProjectCard = ({project : {projectTitle, imageSrc, description, skills, demo, source, tituloProyecto, descripcion}, t, i18n}) => {
   return (
     <div className={styles.container}>
         <img
             src={getImageUrl(imageSrc)}
-            alt={`Image of ${title}`}
+            alt={`Image of ${projectTitle}`}
             className={styles.image}
         />
-        <h3 className={styles.title}>{title}</h3>
-        <p className={styles.description}>{description}</p>
+        <h3 className={styles.title}>{`${i18n.language === 'es' ? t(`${tituloProyecto}`) : t(`${projectTitle}`)}`}</h3>
+        <p className={styles.description}>{`${i18n.language === 'es' ? t(`${descripcion}`) : t(`${description}`)}`}</p>
         <ul className={styles.skills}>
             {
                 skills.map((skill, id) => {
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, {useContext} from 'react'
 
 import styles from './Projects.module.css';
 import projects from '../../data/projects.json';
 import { ProjectCard } from './ProjectCard.jsx';
 
+import { TranslationContext } from '../TranslationContext/TranslationContext.jsx';
+
 export const Projects = () => {
+
+    const { t, i18n } = useContext(TranslationContext);
+
   return (
     <section className={styles.container} id='projects'>
-        <h2 className={styles.title}>Projects</h2>
+        <h2 className={styles.title}>{t('projects.title')}</h2>
         <div className={styles.projects}>
             {
                 projects.map((project, id) => {
                     return (
-                        <ProjectCard key={id} project={project}/>
+                        <ProjectCard
+                            key={id}
+                            project={project}
+                            t={t}
+                            i18n={i18n}
+                        />
                     );
                 })
             }
